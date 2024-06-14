52926a97ef2dc43804378f14ec1a109c0978192c
Daniel Dowdle <dandoodle956@gmail.com>
2024-06-13 19:08:22 -0500

    Boot strapped with shadcn/tailwind

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+
+})
