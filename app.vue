5f6bf44bdc3fbbad1c1e941ef0105e2055d5a3e4
Daniel <dandoodle956@gmail.com>
2024-06-14 21:48:09 -0500

    Added eslint and began working on a projectcard

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-
+    <ProjectCard />
   </div>
 </template>
---
+++ components/project_card/ProjectCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <Card>
+    <CardHeader>
+      <strong> LA Hacks </strong>
+    </CardHeader>
+    <CardContent> Insert product card content here </CardContent>
+    <CardDescription> Insert product card description here </CardDescription>
+    <Button> Hi</Button>
+  </Card>
+</template>
