4e0afc04f2ac79b6552a40809a55d260611f7ed7
Daniel <dandoodle956@gmail.com>
2024-06-16 21:09:20 -0500

    Created a simple project card to be reused for all projects, next steps are small tweaks and populating the project cards

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <ProjectCard />
+  <div
+    style="
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    "
+  >
+    <ProjectCard
+      imageUrl="/img/league_website.png"
+      linkUrl="https://uofm.lol/"
+      title="League of Legend's Intramurals Website"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      :badges="['Nuxt', 'Postgres']"
+    />
+
   </div>
 </template>
--- components/project_card/ProjectCard.vue
+++ components/project_card/ProjectCard.vue
@@ -1,10 +1,87 @@
 <template>
-  <Card>
-    <CardHeader>
-      <strong> LA Hacks </strong>
-    </CardHeader>
-    <CardContent> Insert product card content here </CardContent>
-    <CardDescription> Insert product card description here </CardDescription>
-    <Button> Hi</Button>
+  <Card class="p-0 card">
+    <CardContent class="p-0 project-card">
+      <a :href="linkUrl" target="_blank">
+        <img
+          :src="imageUrl"
+          alt="Project Image"
+          class="project-image"
+        />
+      </a>
+      <CardHeader>
+        <CardTitle>{{ title }}</CardTitle>
+      </CardHeader>
+
+      <p class="p-3">
+        {{ description }}
+      </p>
+      <div class="badges pl-3">
+        <Badge v-for="(badge, index) in badges" :key="index">{{ badge }}</Badge>
+      </div>
+      <div class="buttons">
+        <Button v-for="(button, index) in buttons" :key="index" :href="button.url" target="_blank">{{ button.text }}</Button>
+      </div>
+    </CardContent>
   </Card>
 </template>
+
+<script setup>
+const props = defineProps({
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    linkUrl: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    badges: {
+      type: Array,
+      required: true
+    },
+    buttons: {
+      type: Array,
+      required: false
+    }
+  });
+</script>
+
+<style scoped>
+.project-image {
+  height: auto;
+  display: block;
+  margin: 0 auto;
+  border-radius: 5px 5px 0 0;
+}
+
+.card {
+  margin-bottom: 20px;
+  max-width: 700px;
+}
+
+.project-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.badges {
+  display: flex;
+  align-items: start;
+  gap: 10px;
+  margin-top: 10px;
+  width: 100%;
+}
+
+.buttons {
+  margin-top: 20px;
+}
+</style>
